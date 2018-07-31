0437247f35fad97c0dd24b738fe8d80a59b4a9f1
sunmokcho2 <31606214+sunmokcho2@users.noreply.github.com>
2018-07-31 14:00:13 +0900

    Scenes added in build

--- Test05/Assets/Scripts/MainMenu.cs
+++ Test05/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour {
 
 	public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Level Select");
     }
 
     public void QuitGame()
