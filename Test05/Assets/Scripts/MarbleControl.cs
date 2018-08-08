f2b7ec765d3446157d1430d9f8a2e6595025013c
sunmokcho2 <31606214+sunmokcho2@users.noreply.github.com>
2018-08-08 15:37:52 +0900

    gravity rework

---
+++ Test05/Assets/Scripts/MarbleControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleControl : MonoBehaviour {
+    
+    int marbleSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //GetComponent<Rigidbody2D>().velocity = new Vector2(-marbleSpeed, 0f);
+	}
+}
