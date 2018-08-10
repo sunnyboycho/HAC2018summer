6b2145314eae5ca117ced72a0fc7b74121d28bba
sunmokcho2 <31606214+sunmokcho2@users.noreply.github.com>
2018-08-10 15:30:28 +0900

    save system tentative

---
+++ Test05/Assets/Scripts/ProgressManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressManager : MonoBehaviour {
+
+    public int progress;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Save()
+    {
+        SaveManager.SaveProgress(this);
+    }
+
+    public void Load()
+    {
+        int loadedProgress = SaveManager.LoadProgress();
+
+        progress = loadedProgress;
+    }
+}
---
+++ Test05/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class SaveManager {
+
+    public static void SaveProgress(ProgressManager progressManager)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream stream = new FileStream(Application.persistentDataPath + "/player.sav", FileMode.Create);
+
+        ProgressData data = new ProgressData(progressManager);
+
+        bf.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static int LoadProgress()
+    {
+        if (File.Exists(Application.persistentDataPath + "/player.sav"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream stream = new FileStream(Application.persistentDataPath + "/player.sav", FileMode.Open);
+
+            ProgressData data = bf.Deserialize(stream) as ProgressData;
+
+            stream.Close();
+
+            return data.progress;
+        }
+        else
+        {
+            Debug.LogError("File does not exist.");
+            return 0;
+        }
+    }
+}
+
+[Serializable]
+public class ProgressData
+{
+    public int progress;
+
+    public ProgressData(ProgressManager progressManager)
+    {
+        progress = progressManager.progress;
+    }
+}
